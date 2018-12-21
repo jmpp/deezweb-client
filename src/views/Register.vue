1f33d3397aad934de5d26a785e6ee46a8595cd18
J.M. Cléry <jmclery@gmail.com>
2018-12-21 14:36:30 +0100

    Ajoute le système d'inscription

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,28 +1,41 @@
 <template>
   <main class="container">
     <h1>Inscription</h1>
-    <form>
+
+    <div class="alert alert-danger" role="alert" v-if="errors">
+      Des erreurs ont été rencontrées :
+      <ul>
+        <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+      </ul>
+    </div>
+
+    <div class="alert alert-success" role="alert" v-if="success">
+      {{ success }}
+    </div>
+
+    <form @submit.prevent="registerUser">
       <div class="form-group">
         <label for="nom">Nom</label>
-        <input type="nom" class="form-control" id="nom" placeholder="SMITH">
+        <input type="nom" class="form-control" v-model="nom" id="nom" placeholder="SMITH">
       </div>
       <div class="form-group">
         <label for="prenom">Prénom</label>
-        <input type="prenom" class="form-control" id="prenom" placeholder="John">
+        <input type="prenom" class="form-control" v-model="prenom" id="prenom" placeholder="John">
       </div>
       <div class="form-group">
         <label for="email">Adresse email</label>
-        <input type="email" class="form-control" id="email" placeholder="john.smith@domain.tld">
+        <input type="email" class="form-control" v-model="email" id="email" placeholder="john.smith@domain.tld">
       </div>
       <div class="form-group">
         <label for="motdepasse">Mot de passe</label>
-        <input type="password" class="form-control" id="motdepasse" placeholder="************">
+        <input type="password" class="form-control" v-model="motdepasse" id="motdepasse" placeholder="************">
       </div>
       <div class="form-group">
         <label for="motdepasse_confirmation">Confirmez le mot de passe</label>
         <input
           type="password"
           class="form-control"
+          v-model="motdepasse_confirmation"
           id="motdepasse_confirmation"
           placeholder="************"
         >
@@ -37,7 +50,39 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "Register"
+  name: 'Register',
+
+  data() {
+    return {
+      nom: '',
+      prenom: '',
+      email: '',
+      motdepasse: '',
+      motdepasse_confirmation: '',
+      errors: null,
+      success: null,
+    };
+  },
+
+  methods: {
+    registerUser() {
+      this.errors = null;
+      this.success = null;
+      axios.post('http://localhost:9000/register', {
+        firstname: this.prenom,
+        lastname: this.nom,
+        email: this.email,
+        password: this.motdepasse,
+        password_confirmation: this.motdepasse_confirmation,
+      }).then((response) => {
+        this.success = response.data.message;
+      }).catch((errors) => {
+        this.errors = errors.response.data;
+      });
+    },
+  },
 };
 </script>
